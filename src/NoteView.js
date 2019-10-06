8cfd8c04f910f45477e80d20f425bc6776248b6d
Joe Attardi <joe@attardi.net>
2019-10-05 22:41:35 -0400

    Open links in default browser

--- src/NoteView.js
+++ src/NoteView.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactMarkdown from 'react-markdown';
 import styled from 'styled-components';
 
+const { shell } = window.require('electron');
+
 const Title = styled.h1`
   border-bottom: 1px solid #CCCCCC;
 `;
@@ -10,7 +12,23 @@ export default function NoteView({ title, content }) {
   return (
     <div>
       <Title>{title || 'Untitled'}</Title>
-      <ReactMarkdown source={content} />
+      <ReactMarkdown
+        source={content}
+        renderers={{
+          link: Link
+        }}
+      />
     </div>
+  );
+}
+
+function Link({ href, children }) {
+  function openLink(event) {
+    event.preventDefault();
+    shell.openExternal(event.target.href);
+  }
+
+  return (
+    <a onClick={openLink} href={href}>{children}</a>
   );
 }
